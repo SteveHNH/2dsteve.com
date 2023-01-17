299ec2e263e1ba48b1ce18ddb2622ac20280a8b6
Stephen Adams <tsadams@gmail.com>
2023-01-17 10:08:53 -0500

    Update new_post script to accept title argument
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- new_post.py
+++ new_post.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python
 
+import argparse
 from datetime import date
 from time import strftime
 
-today = date.today()
-filename = today.strftime("%Y-%m-%d") + "-NEW-POST.md"
-url = today.strftime("%Y/%m/%d")
-post_time = strftime("%Y-%m-%d %H:%M:%S+00:00")
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--title')
+args = parser.parse_args()
 
-front_matter = ["---\n",
-"author: Stephen Adams\n",
-"date: " + post_time + "\n",
-"draft: false\n",
-"title: NEW-POST\n",
-"type: post\n",
-"url: /" + url + "/NEW-POST/\n",
-"#image: /img/some_featured_image.jpg\n",
-"#tags:\n",
-"#LIST: books,code,comics,everything,fatherhood,gadgets,games,internet,life,movies,music,nerd,podcasting,politics,random,science,tech,tv,video,work,writing\n",
-"---\n"]
+def main():
+    TITLE = "NEW-POST"
+    if args.title:
+        TITLE = args.title
+    today = date.today()
+    filename = today.strftime("%Y-%m-%d") + f"-{TITLE}.md"
+    url = today.strftime("%Y/%m/%d")
+    post_time = strftime("%Y-%m-%d %H:%M:%S+00:00")
+    
+    front_matter = ["---\n",
+    "author: Stephen Adams\n",
+    "date: " + post_time + "\n",
+    "draft: false\n",
+    f"title: {TITLE}\n",
+    "type: post\n",
+    "url: /" + url + f"/{TITLE}/\n",
+    "#image: /img/some_featured_image.jpg\n",
+    "#tags:\n",
+    "#LIST: books,code,comics,everything,fatherhood,gadgets,games,internet,life,movies,music,nerd,podcasting,politics,random,science,tech,tv,video,work,writing\n",
+    "---\n"]
+    
+    with open("content/posts/" + filename, "w") as f:
+        f.writelines(front_matter)
+    
+    print("post " + filename + " created")
 
-with open("content/posts/" + filename, "w") as f:
-    f.writelines(front_matter)
-
-print("post " + filename + " created")
-
+if __name__ == "__main__":
+    main()
